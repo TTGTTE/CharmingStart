5499ce72b1417415af68c01313a007d8f6ef797a
TTGTTE <keamtollin@gmail.com>
2022-06-25 20:14:13 +0500

    Update first.lua

--- first.lua
+++ first.lua
@@ -1,3 +1,11 @@
 -- Comment
 -- Russian language is not avaible in FUCKING Lua?
 -- Woah
+
+--[[
+Few comments here
+And here
+And even here
+]]
+
+print("Woah Woah Woah"); --[[Typical printf like in C what?!]]
