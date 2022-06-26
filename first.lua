ee7bcdf9da2fc67f7e293971ee8c3bd987feb7b7
TTGTTE <keamtollin@gmail.com>
2022-06-26 10:29:34 +0500

    Кодировка UTF-8
    Пытаюсь настроить кодировку...

--- first.lua
+++ first.lua
@@ -2,11 +2,17 @@
 -- Russian language is not avaible in FUCKING Lua?
 -- Woah
 
+-- Проверка Русского ЯЗЫКА!
+
 --[[
 Few comments here
 And here
 And even here
 ]]
 
+os.setlocale('rus_rus.866')
+
 print("Woah Woah Woah"); --[[Typical printf like in C what?!]]
 print("Don't mind my comment here!");
+
+print('Нет!');
