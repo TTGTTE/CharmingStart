46a3097419554a67f9b2d9761045676e1bf532ba
TTGTTE <keamtollin@gmail.com>
2022-06-26 14:11:52 +0500

    Update first.lua

--- TestLua/first.lua
+++ TestLua/first.lua
@@ -2,12 +2,14 @@
 -- Делаем массив случайных чисел от 1 до 10
 -- Нечётные числа умножаем на 2
 -- Чётные числа делим на 2
+-- Числа меньшие n вывести на экран
 
 
 
 -- Переменные
 local a = {}
 local d = 5
+local n = 5
 
 -- Изначальный массив
 print("Our array:")
@@ -26,7 +28,14 @@ for i = 1, d do
     else
         a[i] = a[i] * 2
     end
+    math.abs(a[i])
     print(a[i])
 end
 
 -- Сортировка
+print("Less than 5:")
+for i = 1, d do
+    if a[i] < 5 then
+    print(a[i])
+    end
+end
