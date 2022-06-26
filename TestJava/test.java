87b076e21e69c99c61d6cf553305fe8d703fcf72
TTGTTE <keamtollin@gmail.com>
2022-06-26 15:16:46 +0500

    Update test.java

--- TestJava/test.java
+++ TestJava/test.java
@@ -3,6 +3,10 @@ package TestJava;
 public class test {
     public static void main(String args[])
     {
-        System.out.println("Мир!");
+        System.out.println("Привет, мир!");
+        System.out.println("Здесь тоже работает нормально кодировка!");
+        System.out.println("Хотя бы так...");
+        System.out.println("А то я устал от lua.");
+        System.out.println("Но, говорят, он быстрее, но вот бы выучить Java.");
     }
 }
