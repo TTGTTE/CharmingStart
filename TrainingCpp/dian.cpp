4e3de2bea5afc651a76b76252878ebe3a737f539
TTGTTE <keamtollin@gmail.com>
2022-06-27 19:17:53 +0500

    Update dian.cpp

--- TrainingCpp/dian.cpp
+++ TrainingCpp/dian.cpp
@@ -1,23 +1,41 @@
 #include <iostream>
 #include <ctime>
+#include <windows.h>
 
 using namespace std;
+
+double random(double min, double max)
+{
+    return (double)(rand())/RAND_MAX*(max - min) + min;
+}
 
 int main()
 {
     int n;
+    float d,q;
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
     cout << "Введите размер: ";
     cin >> n;
 
+    cout << "Случайные числа" << endl;
+    cout << "От: ";
+    cin >> d;
+    cout << "До: ";
+    cin >> q;
+
+
     srand(time(0));
 
-    int a[n];
+    float a[n];
 
     cout << "Изначальный массив:" << endl;
 
     for(int i = 0; i < n; i++)
     {
-        a[i] = (rand() % 15) - 10;
+        a[i] = random(d,q);
         cout << "[" << i << "] = " << a[i] << endl;
     }
 
